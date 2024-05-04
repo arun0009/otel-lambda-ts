a84d31e300e7b64424c05e1fbd096ab1db4df1e0
Arun Gopalpuri <arun@gopalpuri.com>
2024-05-03 23:47:58 -0700

    working span version

--- src/lib/lambda-wrapper.ts
+++ src/lib/lambda-wrapper.ts
@@ -1,50 +1,87 @@
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
-import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
+import { NodeTracerConfig, NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import {
   BatchSpanProcessor,
-  SimpleSpanProcessor,
   ConsoleSpanExporter,
-} from "@opentelemetry/sdk-trace-base";
-import { AwsLambdaInstrumentation } from "@opentelemetry/instrumentation-aws-lambda";
-import { getEnv } from "@opentelemetry/core";
-import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { awsLambdaDetector } from "@opentelemetry/resource-detector-aws";
+  SDKRegistrationConfig,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import { Instrumentation, registerInstrumentations } from '@opentelemetry/instrumentation';
+import { awsLambdaDetector } from '@opentelemetry/resource-detector-aws';
 import {
   detectResourcesSync,
   envDetector,
   processDetector,
-} from "@opentelemetry/resources";
+} from '@opentelemetry/resources';
+import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk';
+import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import {
-  NodeTracerConfig,
-  NodeTracerProvider,
-} from "@opentelemetry/sdk-trace-node";
-import {
-  MeterProvider,
-  MeterProviderOptions,
-} from "@opentelemetry/sdk-metrics";
+  diag,
+  DiagConsoleLogger,
+  DiagLogLevel,
+} from "@opentelemetry/api";
+import { getEnv } from '@opentelemetry/core';
+import { AwsLambdaInstrumentationConfig } from '@opentelemetry/instrumentation-aws-lambda';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { MeterProvider, MeterProviderOptions } from '@opentelemetry/sdk-metrics';
 
-//Ref: https://raw.githubusercontent.com/open-telemetry/opentelemetry-lambda/main/nodejs/packages/layer/src/wrapper.ts
-
-console.log("Registering OpenTelemetry SDK");
+function defaultConfigureInstrumentations() {
+  // Use require statements for instrumentation to avoid having to have transitive dependencies on all the typescript
+  // definitions.
+  const { DnsInstrumentation } = require('@opentelemetry/instrumentation-dns');
+  const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+  const { GraphQLInstrumentation } = require('@opentelemetry/instrumentation-graphql');
+  const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
+  const { HapiInstrumentation } = require('@opentelemetry/instrumentation-hapi');
+  const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+  const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
+  const { KoaInstrumentation } = require('@opentelemetry/instrumentation-koa');
+  const { MongoDBInstrumentation } = require('@opentelemetry/instrumentation-mongodb');
+  const { MySQLInstrumentation } = require('@opentelemetry/instrumentation-mysql');
+  const { NetInstrumentation } = require('@opentelemetry/instrumentation-net');
+  const { PgInstrumentation } = require('@opentelemetry/instrumentation-pg');
+  const { RedisInstrumentation } = require('@opentelemetry/instrumentation-redis');
+  return [  new DnsInstrumentation(),
+    new ExpressInstrumentation(),
+    new GraphQLInstrumentation(),
+    new GrpcInstrumentation(),
+    new HapiInstrumentation(),
+    new HttpInstrumentation(),
+    new IORedisInstrumentation(),
+    new KoaInstrumentation(),
+    new MongoDBInstrumentation(),
+    new MySQLInstrumentation(),
+    new NetInstrumentation(),
+    new PgInstrumentation(),
+    new RedisInstrumentation(),
+  ]
+}
 
 declare global {
-  const spanProcessor: BatchSpanProcessor;
-} 
+  // in case of downstream configuring span processors etc
+  function configureTracerProvider(tracerProvider: NodeTracerProvider): void
+  function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
+  function configureSdkRegistration(
+    defaultSdkRegistration: SDKRegistrationConfig
+  ): SDKRegistrationConfig;
+  function configureMeter(defaultConfig: MeterProviderOptions): MeterProviderOptions;
+  function configureMeterProvider(meterProvider: MeterProvider): void
+  function configureLambdaInstrumentation(config: AwsLambdaInstrumentationConfig): AwsLambdaInstrumentationConfig
+  function configureInstrumentations(): Instrumentation[]
+}
+
+console.log('Registering OpenTelemetry');
 
 const instrumentations = [
-  new AwsLambdaInstrumentation({
-    disableAwsContextPropagation: false,
-  }),  
   new AwsInstrumentation({
     suppressInternalInstrumentation: true,
-    sqsExtractContextPropagationFromPayload: true,
   }),
+  new AwsLambdaInstrumentation(typeof configureLambdaInstrumentation === 'function' ? configureLambdaInstrumentation({}) : {}),
+  ...(typeof configureInstrumentations === 'function' ? configureInstrumentations: defaultConfigureInstrumentations)()
 ];
 
 // configure lambda logging
-const logLevel = getEnv().OTEL_LOG_LEVEL;
-diag.setLogger(new DiagConsoleLogger(), logLevel);
+const logLevel = getEnv().OTEL_LOG_LEVEL
+diag.setLogger(new DiagConsoleLogger(), logLevel)
 
 // Register instrumentations synchronously to ensure code is patched even before provider is ready.
 registerInstrumentations({
@@ -56,38 +93,51 @@ async function initializeProvider() {
     detectors: [awsLambdaDetector, envDetector, processDetector],
   });
 
-  const config: NodeTracerConfig = {
+  let config: NodeTracerConfig = {
     resource,
   };
-
-  const tracerProvider = new NodeTracerProvider(config);
-
-  // global spanProcessor as we need to access it in the handler (update sam cli to use start-api instead of invoke)
-  (global as any).spanProcessor = new BatchSpanProcessor(
-    new OTLPTraceExporter()
-  );
-
-  tracerProvider.addSpanProcessor(spanProcessor);
-
-  // logging for debug
-  if (logLevel === DiagLogLevel.DEBUG) {
-    tracerProvider.addSpanProcessor(
-      new SimpleSpanProcessor(new ConsoleSpanExporter())
-    );
+  if (typeof configureTracer === 'function') {
+    config = configureTracer(config);
   }
 
+  const tracerProvider = new NodeTracerProvider(config);
+  if (typeof configureTracerProvider === 'function') {
+    configureTracerProvider(tracerProvider)
+  } else {
+    // defaults
+    tracerProvider.addSpanProcessor(
+      new BatchSpanProcessor(new OTLPTraceExporter())
+    );
+  }
+  // logging for debug
+  if (logLevel === DiagLogLevel.DEBUG) {
+    tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+  }
+
+  let sdkRegistrationConfig: SDKRegistrationConfig = {};
+  if (typeof configureSdkRegistration === 'function') {
+    sdkRegistrationConfig = configureSdkRegistration(sdkRegistrationConfig);
+  }
+  tracerProvider.register(sdkRegistrationConfig);
+
+  // Configure default meter provider (doesn't export metrics)
   let meterConfig: MeterProviderOptions = {
     resource,
-  };
+  }
+  if (typeof configureMeter === 'function') {
+    meterConfig = configureMeter(meterConfig);
+  }
 
   const meterProvider = new MeterProvider(meterConfig);
+  if (typeof configureMeterProvider === 'function') {
+    configureMeterProvider(meterProvider)
+  }
 
   // Re-register instrumentation with initialized provider. Patched code will see the update.
   registerInstrumentations({
     instrumentations,
     tracerProvider,
-    meterProvider,
+    meterProvider
   });
 }
-
 initializeProvider();
